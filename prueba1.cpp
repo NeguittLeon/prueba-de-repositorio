8e5636f29e99cdee684769e977efafb9d7b3c915
NeguittLeon <101368789+NeguittLeon@users.noreply.github.com>
2022-04-07 23:55:14 -0500

    primer comentario

---
+++ prueba1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <random>
+void guess_game(int nmin, int nmax);
+int main (int argc, char **argv)
+{
+  int NMIN = 5, NMAX = 50;
+  guess_game(NMIN, NMAX);
+  return 0;
+}
+void guess_game(int nmin, int nmax)
+{
+  int ning = 0;
+  std::random_device rd;  
+  std::mt19937 gen(rd()); 
+  std::uniform_int_distribution<int> distrib(nmin, nmax);
+  const int NUM = distrib(gen); 
+  std::cout << "\nEstoy pensando en un numero entre " << nmin << " y " << nmax << std::endl;
+  std::cout << "\nSerias capaz de avdivinar cual es? " << std::endl;
+  std::cin >> ning; 
+  while (ning != NUM)
+  {  
+  if (ning < NUM)
+  {
+   std::cout << "\nEse numero es menor al que estoy pensando, intenta otra vez" << std::endl;
+   std::cin >> ning;
+  }
+  if (ning > NUM)
+  {
+   std::cout << "\nEse numero es mayor al que estoy pensando, intenta otra vez" << std::endl;
+   std::cin >> ning;
+  }
+  }
+  if (ning == NUM)
+  {
+   std::cout << "\nBien hecho, lograste adivinar el numero que estaba pensando " << std::endl;
+  }
+}
+
